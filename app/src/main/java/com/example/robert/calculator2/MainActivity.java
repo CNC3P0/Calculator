8a7f384c518ffcf00dae650c122ea8cb5c14c975
CNC3P0 <rspanell@hotmail.com>
2017-10-03 10:19:01 -0700

    FINAL

--- app/src/main/java/com/example/robert/calculator2/MainActivity.java
+++ app/src/main/java/com/example/robert/calculator2/MainActivity.java
@@ -1,22 +1,55 @@
-package com.example.robert.calculator2;
+/*
+ * App Name:        Rainbow Calculator
+ * Package Name:    calculator2
+ * Developer:       Robert Spanell
+ * Student ID:      T00597013
+ * Class:           COMP 2160
+ * Date:            2017/10/03
+ *
+ * Comments:        Great exercise for starting out with Android Studio and also reacquainting
+ *                  myself with Java. Admittedly the code ended up a bit of a Frankenstein's
+ *                  monster and it needs better encapsulation.
+ *                  If I could start over I probably would have tried a different approach,
+ *                  perhaps Reverse Polish notation, instead of coming up with my own algorithm.
+ *
+ * Dedication:      As one of my first real apps this is for my 8-year-old daughter Khoen. Her
+ *                  point of reference is, of course, games on her Android tablet. Of course she
+ *                  wants me to learn how to design them. The name of my calculator, as well as
+ *                  some of the colour scheme, is her input.
+ */
 
+package com.example.robert.calculator2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+/*
+ * This class implements a simple calculator that does addition, subtraction, multiplication
+ * and division. It supports the decimal point and the negative sign. It also has a selection
+ * of additional feature buttons. These are:  x² (squared), √ (square root), 1/x (inverse),
+ * % (percent), +/- (change sign) and |x| (absolute value).
+ * It supports accumulative processing only, not standard mathematical order of preference,
+ * except as a side effect for the additional features as these are done inline when pressed.
+ * The limit to the number you can type in is somewhere close to 1.8*10^308, so probably your
+ * fingers will tire first. More important considerations are that the screen will only display
+ * 51 characters at a time (3 screen lines, additional characters you may enter are off screen),
+ * and that precision is lost when the answer is converted to scientific notation and displayed
+ * on one line (17 characters).
+ */
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView output;
 
-    boolean equalsIsActive = false;     // Flag for whether previous button was equals
-    boolean operatorlsActive = false;   // Flag for whether previous button was an operator
+    boolean equalsIsActive = false;     // Flag for whether previous button was equals.
+    boolean operatorlsActive = false;   // Flag for whether previous button was an operator.
     char previousOperator = '+';
     char currentOperator;
     double accumulator = 0;
     double currentValue;
-    StringBuffer input = new StringBuffer();
-    CharSequence currentOutput;
+    StringBuffer input = new StringBuffer();    // Someplace to hold what's been entered.
+    CharSequence currentOutput;         // Someplace to retrieve what's on the display into.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +59,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void processBinary(char operator) {
+
+        /*
+         * A subroutine that is called from ifBinary and ifUnary methods.
+         */
+
         switch (operator) {
             case '+':
                 accumulator += currentValue;
@@ -43,6 +81,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void ifDigit(char b) {
+
+        /*
+         * Executes if any digit is pressed.
+         * First checks if last thing pressed was the equals button. If it was then it
+         * resets things since this means a new operation has begun.
+         */
+
         if (equalsIsActive) {
             accumulator = 0;
             equalsIsActive = false;
@@ -53,6 +98,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void ifDecimal(char b) {
+
+        /*
+         * Executes if the decimal is pressed.
+         * First checks if last thing pressed was the equals button. If it was then it
+         * resets things since this means a new operation has begun.
+         * Checks if more than one decimal in a row has been pressed and suppresses them.
+         */
+
         if (equalsIsActive) {
             accumulator = 0;
             equalsIsActive = false;
@@ -66,9 +119,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void ifBinary(char b) {
+
+        /*
+         * Executes if any binary operator is pressed.
+         * First checks if there is no active operation. If there is, then just outputs operator to
+         * display and sets flag.
+         */
+
         equalsIsActive = false;
         if (!operatorlsActive) {
-            operatorlsActive = true;
+            operatorlsActive = true;    // I think this is redundant but didn't want to risk
+                                        // breakage so close to deadline.
             if (currentOperator == 0) {
                 previousOperator = '+';
             } else {
@@ -86,6 +147,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void ifUnary(char b) {
+
+        /*
+         * Executes if any unary operator is pressed.
+         * First checks if there is no active operation. If there is, then does nothing.
+         * Performs required operation on the last entered value and replaces it on the screen so
+         * that the next operation can proceed.
+         */
+
         if (!operatorlsActive) {
             //operatorlsActive = true;
             if (currentOperator == 0) {
@@ -177,9 +246,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void ifEquals() {
+
+        /*
+         * Executes if equals is pressed.
+         */
+
         if (input.length() != 0) {
             currentValue = Double.parseDouble(input.toString());
-            switch (currentOperator) {
+            switch (currentOperator) {  // I think I can use modify processBinary method here but
+                                        // didn't want to risk breakage so close to deadline.
                 case '+':
                     accumulator += currentValue;
                     break;
@@ -206,7 +281,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View v) { //Monitor button presses and trigger events
+    public void onClick(View v) {
+
+        /*
+         * Monitors button presses and trigger events
+         */
+
         switch (v.getId()) {
 
             //digits
